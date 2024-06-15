b14e0dad9c60d7d7c53419040017690355a22c0f
LoicLab <labas-loic@protonmail.com>
2024-06-15 16:45:18 +0200

    Update list

---
+++ lib/screens/modify_list_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_list/widgets/list_form.dart';
+
+import '../providers/list_provider.dart';
+
+class ModifyListScreen extends StatelessWidget {
+  final TargetPlatform platform;
+  final String titleBar;
+  final int listId;
+
+  const ModifyListScreen({
+    super.key,
+    required this.platform,
+    required this.titleBar,
+    required this.listId
+  });
+
+  bool isAndroid() => (platform == TargetPlatform.android);
+
+  Widget scaffold(BuildContext context) {
+    return (isAndroid())
+        ? Scaffold(
+        appBar: AppBar(title: Text(titleBar), backgroundColor: Theme.of(context).colorScheme.primary),
+        body: body(context: context)
+    )
+        : CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+            middle: Text(titleBar),
+            backgroundColor: Theme.of(context).colorScheme.primary
+        ),
+        child: body(context: context)
+    );
+  }
+
+  Widget body({required BuildContext context}){
+    return ListForm(
+        submitButton: ElevatedButton(
+            onPressed: (){
+              context.read<ListProvider>().update(listId: listId);
+              Navigator.of(context).pop();
+            },
+            child: const Text('Modifier')
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.read<ListProvider>().getListById(listId: listId);
+    return scaffold(context);
+  }
+
+}
