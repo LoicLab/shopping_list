845110dc7bd6c7f7e7d35110579c08992fd61839
LoicLab <labas-loic@protonmail.com>
2024-09-19 08:01:03 +0200

    Added search in a list

--- lib/providers/items_provider.dart
+++ lib/providers/items_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_list/models/item.dart';
 import 'package:shopping_list/services/item_repository.dart';
@@ -5,31 +6,66 @@ import 'package:shopping_list/services/list_repository.dart';
 
 ///Items provider
 class ItemsProvider with ChangeNotifier {
+  ///Items list
   late List<Item> items = [];
+  ///Items list filtered for search
+  late List<Item> _filteredItems = [];
   late Item item;
-  late int listId;
+  TextEditingController searchValue = TextEditingController();
+
+  List<Item> get filteredItems => _filteredItems.isEmpty ? items : _filteredItems;
 
   ///Update status of item
   updateItemStatus({required int index}){
     //Change status
-    Item item = items[index];
+    Item item = _filteredItems[index];
     item.status = !item.status;
     //Update database
-    ItemRepository().updateItem(item: items[index]);
+    ItemRepository().updateItem(item: _filteredItems[index]);
     notifyListeners();
   }
 
   ///Remove item
   removeItem({required int index}){
-    ItemRepository().removeItemById(itemId: items[index].id!);
+    ItemRepository().removeItemById(itemId: _filteredItems[index].id!);
     //Current list remove item
-    items.removeAt(index);
+    _filteredItems.removeAt(index);
     notifyListeners();
   }
 
+  ///Get items by list id
   Future<void> getItemsByListId({required int listId}) async {
-    items = await ListRepository().getItemsByListId(listId: listId);
+    List<Item> newItems = await ListRepository().getItemsByListId(listId: listId);
+    //For add new item and refresh the list
+    if(newItems.length > items.length){
+      _filteredItems = newItems;
+    }
+    items = newItems;
+    //If change of list reset search
+    if(
+      _filteredItems.isNotEmpty && _filteredItems.first.itemListId != listId ||
+      _filteredItems.isEmpty
+    ){
+      resetSearch();
+    }
     notifyListeners();
   }
 
+  ///Search items in list by query
+  void searchItems({required String query}) {
+    if (query.isEmpty) {
+      _filteredItems = items;
+    } else {
+      _filteredItems = items
+          .where((item) => item.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    }
+    notifyListeners();
+  }
+
+  void resetSearch() {
+    searchValue.clear();
+    _filteredItems = items;
+    notifyListeners();
+  }
 }
